8524eae636d261c2e8260a604fbf952042cd2a56
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2024-05-18 14:06:25 +0530

    fixed css and text aligned

--- src/components/CategoryDetails.jsx
+++ src/components/CategoryDetails.jsx
@@ -14,27 +14,22 @@ const CategoryDetails = () => {
   }
 
   return (
-    <div className="container">
-      <center>
-        <div className="bg-custom-color-1 py-30 border-2 hover:border-green-500 transform transition duration-500 hover:scale-105 animate-fadeIn">
-          <a
-            href="/"
-            className="flex flex-col justify-evenly items-center border-gray-500 rounded-lg shadow md:flex-row md:max-w-xl dark:border-gray-700 dark:bg-custom-color-1 bg-custom-color-1 transition duration-500 hover:shadow-lg"
-          >
-            <img
-              className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg transition duration-500 hover:opacity-1000"
-              src={category.strCategoryThumb}
-              alt=""
-            />
-            <div className="flex flex-col justify-between p-4 leading-normal">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-green-900 transition duration-500 hover:text-green-500">
-                {category.strCategory}
-              </h5>
-              <p className="mb-3 font-normal text-green-700 dark:text-gray-400 transition duration-500 hover:text-red-900 hover:font-serif hover:font-semibold">
-                {category.strCategoryDescription}
-              </p>
-            </div>
-          </a>
+    <div className="container bg-custom-color-1 ">
+      <center className="">
+        <div className="py-30 transform transition duration-500 hover:scale-105 animate-fadeIn max-w-3xl text-wrap">
+          <img
+            className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg transition duration-500 hover:opacity-200 "
+            src={category.strCategoryThumb}
+            alt=""
+          />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-green-900 transition duration-500 hover:text-green-500">
+              {category.strCategory}
+            </h5>
+            <p className="mb-3 font-normal text-green-700 dark:text-gray-400 transition duration-500 hover:text-red-900 hover:font-serif hover:font-semibold">
+              {category.strCategoryDescription}
+            </p>
+          </div>
         </div>
       </center>
     </div>
